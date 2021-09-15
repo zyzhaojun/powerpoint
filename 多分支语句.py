3db069ed8fd35a19bef0bae7ed6a93552c170871
zyzhaojun <zyzhaojun@gmail.com>
2021-09-15 10:36:06 +0800

    Create 多分支语句.py

---
+++ 多分支语句.py
@@ -0,0 +1,12 @@
+age=int(input('请输入年龄：'))
+if age <= 6:
+    print('童年')
+elif age <= 17:
+    print('少年')
+elif age <=40:
+    print('青年')
+elif age <= 65:
+    print('中年')
+else:
+    print('老年')
+
